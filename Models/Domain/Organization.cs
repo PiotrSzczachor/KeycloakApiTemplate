f1552b4c78fc9f6c93d255f2241d65bfc9d6dd4a
mikolaj.mendak <mikomen11@gmail.com>
2025-10-04 20:12:26 +0200

    optional etntities and json ignore

--- Models/Domain/Organization.cs
+++ Models/Domain/Organization.cs
@@ -1,13 +1,17 @@
-﻿namespace Models.Domain
+﻿using System.Text.Json.Serialization;
+
+namespace Models.Domain
 {
     public class Organization
     {
         public string? Name { get; set; }
         public Guid Guid { get; set; }
-        public required Guid UserGuid { get; set; }
-        public required User User { get; set; }
-        public required Guid AddressGuid { get; set; }
-        public required Address Address { get; set; }
+        public Guid? UserGuid { get; set; }
+        [JsonIgnore]
+        public User? User { get; set; }
+        public Guid? AddressGuid { get; set; }
+        [JsonIgnore]
+        public Address? Address { get; set; }
     }
 
 }
--- Models/Domain/School.cs
+++ Models/Domain/School.cs
@@ -1,11 +1,15 @@
-﻿namespace Models.Domain
+﻿using System.Text.Json.Serialization;
+
+namespace Models.Domain
 {
     public class School
     {
         public Guid Guid { get; set; }
+        [JsonIgnore]
         public Address? Address { get; set; }
         public Guid? AddressGuid { get; set; }
         public string? Name { get; set; }
+        [JsonIgnore]
         public ICollection<User> Users { get; set; } = new List<User>();
     }
 }
