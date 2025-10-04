a6600a148bb2461c41c2e96513fc37ab83c59872
mikolaj.mendak <mikomen11@gmail.com>
2025-10-04 20:39:00 +0200

    udpate profile and record

--- Models/Profiles/EventMappingProfile.cs
+++ Models/Profiles/EventMappingProfile.cs
@@ -9,9 +9,9 @@ namespace Models.Profiles
         public EventMappingProfile()
         {
             CreateMap<Event, EventDto>()
-                .ForMember(dest => dest.OrganizationName, opt => opt.MapFrom(src => src.Organization.User.Name))
-                .ForMember(dest => dest.AddressCity, opt => opt.MapFrom(src => src.Address.City))
-                .ForMember(dest => dest.AddressStreet, opt => opt.MapFrom(src => src.Address.Street))
+                .ForMember(dest => dest.OrganizationName, opt => opt.MapFrom(src => src.Organization != null ? src.Organization.User.Name : string.Empty))
+                .ForMember(dest => dest.AddressCity, opt => opt.MapFrom(src => src.Address != null ? src.Address.City : string.Empty))
+                .ForMember(dest => dest.AddressStreet, opt => opt.MapFrom(src => src.Address != null ? src.Address.Street : string.Empty))
                 .ForMember(dest => dest.Participants, opt => opt.MapFrom(src => src.UserEvents.Select(ue => ue.User)));
 
         }
